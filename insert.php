653db7ec67e28b92fe90fc6549f2784bb6e967fc
Milan Mijatov <milanmijatov0@gmail.com>
2019-03-09 21:51:38 +1100

    Checking the command line options

--- insert.php
+++ insert.php
@@ -1,3 +1,9 @@
 <?php
     $options = getopt("u:h:p:", array("help","file:","dry_run","create_table",));
+    $user = isset($options['u']);
+    $pass = isset($options['p']);
+    $file = isset($options['file']);
+    $create_table = isset($options['create_table']);
+    $dry_run = isset($options['dry_run']);
+    $help = isset($options['help']);
 ?>
