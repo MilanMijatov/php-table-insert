0b1c230203ea9cd26e7b1dfc98ab79d8142075a5
Milan Mijatov <milanmijatov0@gmail.com>
2019-03-12 15:58:30 +1100

    Added help

--- insert.php
+++ insert.php
@@ -8,6 +8,18 @@
     $dry_run = isset($options['dry_run']);
     $help = isset($options['help']);
 
+    if($help === true) {
+        exit("--file [csv file name] – this is the name of the CSV to be parsed
+--create_table – this will cause the MySQL users table to be built (and no further action will be taken)
+--dry_run – this will be used with the --file directive in the instance that we want
+            to run the script but not insert into the DB. All other functions will be executed,
+            but the database won't be altered.
+-u – MySQL username
+-p – MySQL password
+-h – MySQL host
+--help – which will output the above list of directives with details.");
+    }
+
     if($user === false || $host === false) {
         exit("Error Username and Host required");
     }
