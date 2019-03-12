eb1f9144869277050accd88654f70f1d5b2344fd
Milan Mijatov <milanmijatov0@gmail.com>
2019-03-12 15:55:47 +1100

    Syntax change

--- insert.php
+++ insert.php
@@ -2,12 +2,13 @@
     $options = getopt("u:h:p:", array("help","file:","dry_run","create_table",));
     $user = isset($options['u']);
     $pass = isset($options['p']);
+    $host = isset($options['h']);
     $file = isset($options['file']);
     $create_table = isset($options['create_table']);
     $dry_run = isset($options['dry_run']);
     $help = isset($options['help']);
 
-    if($username === false || $host === false) {
-        exit "Error Username and Host required\n";
+    if($user === false || $host === false) {
+        exit("Error Username and Host required");
     }
 ?>
